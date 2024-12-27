5f02f11938a17de1d2acbda9b2aa5a4158c6f3ac
Krishnkant modi <modikishu41@gmail.com>
2024-12-27 14:50:08 +0530

    Initially Setup Completely

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        // Ignore specific warnings
+        if (warning.code === 'SOME_WARNING_CODE') return;
+        warn(warning); // Default behavior
+      },
+    },
+  },
+});
 
 
